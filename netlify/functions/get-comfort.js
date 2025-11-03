288ece6004a2d0307249c4a23c1a62a7b99a0dd1
ajent7 <anurag.gangwal.07@gmail.com>
2025-11-04 02:50:02 +0530

    Update get-comfort.js

--- netlify/functions/get-comfort.js
+++ netlify/functions/get-comfort.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
     const { feelings } = JSON.parse(event.body);
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: 'POST',
         headers: {
